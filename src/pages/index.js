27d6dc9b1094b98b411c14ecf7d916406c0298e8
rosinrae <rosalynrdecker@gmail.com>
2018-05-30 14:50:13 -0600

    added stub pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Link from '../components/link'
 import BasicPage from '../components/basic-page'
 
 const IndexPage = () => (
   <BasicPage title="">
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
   </BasicPage>
 )
 
