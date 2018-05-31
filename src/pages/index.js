de538872b8d5b23b9b50a4cb22754a6495bd964f
rosinrae <rosalynrdecker@gmail.com>
2018-05-31 14:34:43 -0600

    emptied main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import BasicPage from '../components/basic-page'
 
 const IndexPage = () => (
   <BasicPage title="">
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
   </BasicPage>
 )
 
