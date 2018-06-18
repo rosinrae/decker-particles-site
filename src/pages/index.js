a114695f9647c27d7bb05e29ce575782bd84a1c4
rosinrae <rosalynrdecker@gmail.com>
2018-06-18 13:10:09 -0600

    added thumbnails

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import BasicPage from '../components/basic-page'
 import Gallery from '../components/gallery'
-import blackWhiteFlake from '../assets/black-white-flake.jpg'
-import whiteBlackFlake from '../assets/white-black-flake.jpg'
-import candySpeckle from '../assets/candy-speckle.jpg'
-import stoneSpeckle from '../assets/stone-speckle.jpg'
-import fryingPanBase from '../assets/frying-pan-base.jpg'
+import img1 from '../assets/black-white-flake-large.jpg'
+import img2 from '../assets/white-black-flake-large.jpg'
+import img3 from '../assets/candy-speckle-large.jpg'
+import img4 from '../assets/stone-speckle-large.jpg'
+import img5 from '../assets/frying-pan-base-large.jpg'
 
-const imageProps = (image) => {
+import thumb1 from '../assets/thumbnails/black-white-flake.png'
+import thumb2 from '../assets/thumbnails/white-black-flake.png'
+import thumb3 from '../assets/thumbnails/candy-speckle.png'
+import thumb4 from '../assets/thumbnails/stone-speckle.png'
+import thumb5 from '../assets/thumbnails/frying-pan-base.png'
+
+const thumbnails = [thumb1, thumb2, thumb3, thumb4, thumb5]
+
+const imageProps = (image, index) => {
   let filename = image.split('/').pop()
   let alt = (filename.split('.')[0]).replace(/-/g, ' ')
+  let thumbnail = thumbnails[index]
   return {
     original: image,
-    originalAlt: alt 
+    originalAlt: alt,
+    thumbnail: thumbnail
   }
 }
 
-const images = [blackWhiteFlake, whiteBlackFlake, candySpeckle, stoneSpeckle, fryingPanBase].map(imageProps)
+const images = [img1, img2, img3, img4, img5].map(imageProps)
 
 const IndexPage = () => (
   <BasicPage title="">
