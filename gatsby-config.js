e00be40108b2bccd47817cd7cc4b96ffe5c26702
rosinrae <rosalynrdecker@gmail.com>
2018-06-17 18:21:02 -0600

    minimal netlify cms setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Decker Particles',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+            'gatsby-plugin-netlify-cms'],
 }
