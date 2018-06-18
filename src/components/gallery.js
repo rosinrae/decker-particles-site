24771d3f52a8ae7be0f5031a40356fd2e72fdcb9
rosinrae <rosalynrdecker@gmail.com>
2018-06-17 19:22:06 -0600

    added image gallery

---
+++ src/components/gallery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ImageGallery from 'react-image-gallery'
+import styled from 'styled-components'
+
+const Gallery = styled(ImageGallery)`
+
+`
+
+export default Gallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import BasicPage from '../components/basic-page'
+import Gallery from '../components/gallery'
+import blackWhiteFlake from '../assets/black-white-flake.jpg'
+import whiteBlackFlake from '../assets/white-black-flake.jpg'
+import candySpeckle from '../assets/candy-speckle.jpg'
+import stoneSpeckle from '../assets/stone-speckle.jpg'
+import fryingPanBase from '../assets/frying-pan-base.jpg'
+
+const imageProps = (image) => {
+  let filename = image.split('/').pop()
+  let alt = (filename.split('.')[0]).replace(/-/g, ' ')
+  console.log(alt)
+  return {
+    original: image,
+    originalAlt: alt 
+  }
+}
+
+const images = [blackWhiteFlake, whiteBlackFlake, candySpeckle, stoneSpeckle, fryingPanBase].map(imageProps)
 
 const IndexPage = () => (
   <BasicPage title="">
+    <Gallery items={images} />
   </BasicPage>
 )
 
