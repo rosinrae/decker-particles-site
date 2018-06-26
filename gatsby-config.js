9ede08940161f154a1bcd1fd7e915f4114c02f23
rosinrae <rosalynrdecker@gmail.com>
2018-06-26 13:32:58 -0600

    added resolution path in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,17 @@ module.exports = {
   siteMetadata: {
     title: 'Decker Particles',
   },
-  plugins: ['gatsby-plugin-react-helmet',
-            'gatsby-transformer-sharp',
-            'gatsby-plugin-sharp',
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`
+      }
+    },
   ],
 }
+
